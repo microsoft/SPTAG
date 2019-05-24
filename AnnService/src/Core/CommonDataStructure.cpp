d381ecc097fcf0b74765637d087c67677f2871ca
Nishant Kumar Singh <nishant.itbhu@outlook.com>
2019-05-24 14:04:41 +0530

    Fix assignment operator issues in ByteArray
    + Fix assignment operator issues in ByteArray
    + Add noexcept qualifier to functions

--- AnnService/src/Core/CommonDataStructure.cpp
+++ AnnService/src/Core/CommonDataStructure.cpp
@@ -3,28 +3,29 @@
 
 #include "inc/Core/CommonDataStructure.h"
 
-using namespace SPTAG;
+namespace SPTAG
+{ 
 
 const ByteArray ByteArray::c_empty;
 
-ByteArray::ByteArray()
-    : m_data(nullptr),
-    m_length(0)
+ByteArray::ByteArray() noexcept
+    : m_data(nullptr)
+    , m_length(0)
 {
 }
 
-
-ByteArray::ByteArray(ByteArray&& p_right)
-    : m_data(p_right.m_data),
-    m_length(p_right.m_length),
-    m_dataHolder(std::move(p_right.m_dataHolder))
+ByteArray::ByteArray(ByteArray&& p_right) noexcept
+    : m_data(p_right.m_data)
+    , m_length(p_right.m_length)
+    , m_dataHolder(std::move(p_right.m_dataHolder))
 {
+    p_right.m_data = nullptr;
+    p_right.m_length = 0;
 }
-
 
 ByteArray::ByteArray(std::uint8_t* p_array, std::size_t p_length, bool p_transferOnwership)
-    : m_data(p_array),
-      m_length(p_length)
+    : m_data(p_array)
+    , m_length(p_length)
 {
     if (p_transferOnwership)
     {
@@ -32,101 +33,72 @@ ByteArray::ByteArray(std::uint8_t* p_array, std::size_t p_length, bool p_transfe
     }
 }
 
-
-ByteArray::ByteArray(std::uint8_t* p_array, std::size_t p_length, std::shared_ptr<std::uint8_t> p_dataHolder)
-    : m_data(p_array),
-      m_length(p_length),
-      m_dataHolder(std::move(p_dataHolder))
+ByteArray::ByteArray(std::uint8_t* p_array, std::size_t p_length, std::shared_ptr<std::uint8_t> p_dataHolder) noexcept
+    : m_data(p_array)
+    , m_length(p_length)
+    , m_dataHolder(std::move(p_dataHolder))
 {
 }
 
-
-ByteArray::ByteArray(const ByteArray& p_right)
+ByteArray::ByteArray(const ByteArray& p_right) noexcept
     : m_data(p_right.m_data),
       m_length(p_right.m_length),
       m_dataHolder(p_right.m_dataHolder)
 {
 }
 
-
 ByteArray&
-ByteArray::operator= (const ByteArray& p_right)
+ByteArray::operator=(const ByteArray& p_right) noexcept
 {
-    m_data = p_right.m_data;
-    m_length = p_right.m_length;
-    m_dataHolder = p_right.m_dataHolder;
+    if (this != std::addressof(p_right))
+    {
+        m_data = p_right.m_data;
+        m_length = p_right.m_length;
+        m_dataHolder = p_right.m_dataHolder;
+    }
 
     return *this;
 }
 
-
 ByteArray&
-ByteArray::operator= (ByteArray&& p_right)
+ByteArray::operator=(ByteArray&& p_right) noexcept
 {
-    m_data = p_right.m_data;
-    m_length = p_right.m_length;
-    m_dataHolder = std::move(p_right.m_dataHolder);
+    if (this != std::addressof(p_right))
+    {
+        m_data = p_right.m_data;
+        m_length = p_right.m_length;
+        m_dataHolder = std::move(p_right.m_dataHolder);
+    }
 
     return *this;
 }
-
-
-ByteArray::~ByteArray()
-{
-}
-
 
 ByteArray
 ByteArray::Alloc(std::size_t p_length)
 {
-    ByteArray byteArray;
     if (0 == p_length)
     {
-        return byteArray;
+        return ByteArray();
     }
-
-    byteArray.m_dataHolder.reset(new std::uint8_t[p_length],
-                                 std::default_delete<std::uint8_t[]>());
-
-    byteArray.m_length = p_length;
-    byteArray.m_data = byteArray.m_dataHolder.get();
-    return byteArray;
+    else {
+        auto array = new std::uint8_t[p_length];
+        return ByteArray(array, p_length, true);
+    }
 }
-
-
-std::uint8_t*
-ByteArray::Data() const
-{
-    return m_data;
-}
-
-
-std::size_t
-ByteArray::Length() const
-{
-	return m_length;
-}
-
 
 void
-ByteArray::SetData(std::uint8_t* p_array, std::size_t p_length)
+ByteArray::SetData(std::uint8_t* p_array, std::size_t p_length) noexcept
 {
     m_data = p_array;
-	m_length = p_length;
+    m_length = p_length;
+    m_dataHolder.reset();
 }
-
-
-std::shared_ptr<std::uint8_t>
-ByteArray::DataHolder() const
-{
-    return m_dataHolder;
-}
-
 
 void
-ByteArray::Clear()
+ByteArray::Clear() noexcept
 {
     m_data = nullptr;
     m_dataHolder.reset();
     m_length = 0;
+}
 }
