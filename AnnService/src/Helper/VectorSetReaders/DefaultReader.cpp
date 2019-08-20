ace28c25dd36be096762dc36ffb993c0985a5743
Qi Chen <cheqi@microsoft.com>
2019-08-20 11:03:12 +0800

    fix space issues

--- AnnService/src/Helper/VectorSetReaders/DefaultReader.cpp
+++ AnnService/src/Helper/VectorSetReaders/DefaultReader.cpp
@@ -408,8 +408,8 @@ DefaultReader::MergeData()
 
     outputStream.open(m_vectorOutput, std::ofstream::binary);
 
-	outputStream.write(reinterpret_cast<char*>(&totalRecordCount), sizeof(totalRecordCount));
-	outputStream.write(reinterpret_cast<char*>(&(m_options->m_dimension)), sizeof(m_options->m_dimension));
+    outputStream.write(reinterpret_cast<char*>(&totalRecordCount), sizeof(totalRecordCount));
+    outputStream.write(reinterpret_cast<char*>(&(m_options->m_dimension)), sizeof(m_options->m_dimension));
 
     for (std::uint32_t i = 0; i < m_subTaskCount; ++i)
     {
