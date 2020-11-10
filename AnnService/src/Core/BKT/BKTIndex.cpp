d6deb17ef152f5886ef21ed6741a4495772d9b71
Qi Chen <cheqi@microsoft.com>
2020-11-10 06:58:54 +0000

    fix concurrent delete when index is not ready

--- AnnService/src/Core/BKT/BKTIndex.cpp
+++ AnnService/src/Core/BKT/BKTIndex.cpp
@@ -447,6 +447,8 @@ namespace SPTAG
 
         template <typename T>
         ErrorCode Index<T>::DeleteIndex(const SizeType& p_id) {
+            if (!m_bReady) return ErrorCode::EmptyIndex;
+
             std::shared_lock<std::shared_timed_mutex> sharedlock(m_dataDeleteLock);
             if (m_deletedID.Insert(p_id)) return ErrorCode::Success;
             return ErrorCode::VectorNotFound;
