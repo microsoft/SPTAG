617a8459531051c4a78e19e867b1b8400d0c4b73
Philip Adams <philipadams@microsoft.com>
2021-12-19 14:24:38 -0800

    Implement PQ training

---
+++ AnnService/inc/Quantizer/Training.h
@@ -0,0 +1,133 @@
+#pragma once
+#include <inc/Core/Common/DistanceUtils.h>
+#include <inc/Core/Common/IQuantizer.h>
+#include <inc/Core/Common/PQQuantizer.h>
+
+#include <memory>
+#include <inc/Core/VectorSet.h>
+#include "inc/Core/Common/BKTree.h"
+
+#define blockRows 4096
+using namespace SPTAG;
+
+class QuantizerOptions : public Helper::ReaderOptions
+{
+public:
+    QuantizerOptions(SizeType trainingSamples, bool debug, float lambda) : Helper::ReaderOptions(VectorValueType::Float, 0, VectorFileType::TXT, "|", 32), m_trainingSamples(trainingSamples), m_debug(debug), m_KmeansLambda(lambda)
+    {
+        AddRequiredOption(m_inputFiles, "-i", "--input", "Input raw data.");
+        AddRequiredOption(m_outputFile, "-o", "--output", "Output quantized vectors.");
+        AddRequiredOption(m_outputMetadataFile, "-om", "--outputmetadata", "Output metadata.");
+        AddRequiredOption(m_outputMetadataIndexFile, "-omi", "--outputmetadataindex", "Output metadata index.");
+        AddRequiredOption(m_outputQuantizerFile, "-oq", "--outputquantizer", "Output quantizer.");
+        AddRequiredOption(m_quantizerType, "-qt", "--quantizer", "Quantizer type.");
+        AddRequiredOption(m_quantizedDim, "-qd", "--quantizeddim", "Quantized Dimension.");
+        AddOptionalOption(m_trainingSamples, "-ts", "--train_samples", "Number of samples for training.");
+        AddOptionalOption(m_debug, "-debug", "--debug", "Print debug information");
+        AddOptionalOption(m_KmeansLambda, "-kml", "--lambda", "Kmeans lambda parameter");
+    }
+
+    ~QuantizerOptions() {}
+
+    std::string m_inputFiles;
+
+    std::string m_outputFile;
+
+    std::string m_outputMetadataFile;
+
+    std::string m_outputMetadataIndexFile;
+
+    std::string m_outputQuantizerFile;
+
+    DimensionType m_quantizedDim;
+
+    SizeType m_trainingSamples;
+
+    SPTAG::QuantizerType m_quantizerType;
+
+    bool m_debug;
+
+    float m_KmeansLambda;
+};
+
+template <typename T>
+T* TrainPQQuantizer(std::shared_ptr<QuantizerOptions> options, std::shared_ptr<VectorSet> raw_vectors, std::shared_ptr<VectorSet> quantized_vectors)
+{
+    SizeType numCentroids = 256;
+    if (raw_vectors->Dimension() % options->m_quantizedDim != 0) {
+        LOG(Helper::LogLevel::LL_Error, "Only n_codebooks that divide dimension are supported.\n");
+        exit(1);
+    }
+    DimensionType subdim = raw_vectors->Dimension() / options->m_quantizedDim;
+    ByteArray PQ_vector_array = ByteArray::Alloc(sizeof(std::uint8_t) * options->m_quantizedDim * raw_vectors->Count());
+    quantized_vectors.reset(new BasicVectorSet(PQ_vector_array, VectorValueType::UInt8, options->m_quantizedDim, raw_vectors->Count()));
+    T* codebooks = (T*)_mm_malloc(sizeof(T) * 256 * raw_vectors->Dimension(), ALIGN_SPTAG);
+
+    for (int codebookIdx = 0; codebookIdx < options->m_quantizedDim; codebookIdx++) {
+        auto kargs = COMMON::KmeansArgs<T>(numCentroids, subdim, raw_vectors->Count(), options->m_threadNum, DistCalcMethod::L2);
+        auto dset = COMMON::Dataset<T>(raw_vectors->Count(), subdim, blockRows, raw_vectors->Count());
+
+#pragma omp parallel for
+        for (int vectorIdx = 0; vectorIdx < raw_vectors->Count(); vectorIdx++) {
+            auto raw_addr = reinterpret_cast<T*>(raw_vectors->GetVector(vectorIdx)) + (codebookIdx * subdim);
+            auto dset_addr = dset[vectorIdx];
+            for (int k = 0; k < subdim; k++) {
+                dset_addr[k] = raw_addr[k];
+            }
+        }
+
+        std::vector<SizeType> localindices;
+        localindices.resize(dset.R());
+        for (SizeType il = 0; il < localindices.size(); il++) localindices[il] = il;
+
+        auto nclusters = COMMON::KmeansClustering<T>(dset, localindices, 0, dset.R(), kargs, options->m_trainingSamples, options->m_KmeansLambda, options->m_debug, nullptr);
+
+        std::vector<SizeType> reverselocalindex;
+        reverselocalindex.resize(dset.R());
+        for (SizeType il = 0; il < reverselocalindex.size(); il++)
+        {
+            reverselocalindex[localindices[il]] = il;
+        }
+
+#pragma omp parallel for
+        for (int vectorIdx = 0; vectorIdx < raw_vectors->Count(); vectorIdx++) {
+            auto localidx = reverselocalindex[vectorIdx];
+            auto quan_addr = reinterpret_cast<uint8_t*>(quantized_vectors->GetVector(vectorIdx));
+            quan_addr[codebookIdx] = kargs.label[localidx];
+
+        }
+
+        for (int j = 0; j < numCentroids; j++) {
+            std::cout << kargs.counts[j] << '\t';
+        }
+        std::cout << std::endl;
+
+        T* cb = codebooks + (numCentroids * subdim * codebookIdx);
+        for (int i = 0; i < numCentroids; i++)
+        {
+            for (int j = 0; j < subdim; j++)
+            {
+                cb[i * subdim + j] = kargs.centers[i * subdim + j];
+            }
+        }
+    }
+
+    return codebooks;
+}
+
+ErrorCode WriteQuantizedVecs(std::shared_ptr<VectorSet> vectors, std::shared_ptr<Helper::DiskPriorityIO> fp)
+{
+	SizeType cnt = vectors->Count();
+	DimensionType dim = COMMON::DistanceUtils::Quantizer->GetNumSubvectors();
+	SizeType qvec_size = COMMON::DistanceUtils::Quantizer->QuantizeSize();
+	IOBINARY(fp, WriteBinary, sizeof(SizeType), (char*)&cnt);
+	IOBINARY(fp, WriteBinary, sizeof(DimensionType), (char*)&dim);
+
+	uint8_t* qvec = (uint8_t*) _mm_malloc(qvec_size, ALIGN_SPTAG);
+	for (int i = 0; i < cnt; i++)
+	{
+		COMMON::DistanceUtils::Quantizer->QuantizeVector(vectors->GetVector(i), qvec);
+		IOBINARY(fp, WriteBinary, qvec_size, (char*)qvec);
+	}
+	
+}
--- AnnService/src/Quantizer/main.cpp
+++ AnnService/src/Quantizer/main.cpp
@@ -5,34 +5,75 @@
 #include "inc/Core/VectorIndex.h"
 #include "inc/Core/Common.h"
 #include "inc/Helper/SimpleIniReader.h"
+#include <inc/Core/Common/DistanceUtils.h>
+#include "inc/Quantizer/Training.h"
 
 #include <memory>
 
 using namespace SPTAG;
 
-class QuantizerOptions : public Helper::ReaderOptions
-{
-public:
-    QuantizerOptions() : Helper::ReaderOptions(VectorValueType::Float, 0, VectorFileType::TXT, "|", 32)
-    {
-        AddRequiredOption(m_inputFiles, "-i", "--input", "Input raw data.");
-        AddRequiredOption(m_outputFile, "-o", "--output", "Output quantized vectors");
-        AddRequiredOption(m_outputQuantizerFile, "-oq", "--outputquantizer", "Output quantizer");
-        AddRequiredOption(m_quantizerType, "-qt", "--quantizer", "Quantizer type.");
-    }
-
-    ~QuantizerOptions() {}
-
-    std::string m_inputFiles;
-
-    std::string m_outputFile;
-
-    SPTAG::Quantizer m_indexAlgoType;
-
-    std::string m_builderConfigFile;
-};
-
 int main(int argc, char* argv[])
 {
+    std::shared_ptr<QuantizerOptions> options(new QuantizerOptions(10000, true, 1.0));
+    if (!options->Parse(argc - 1, argv + 1))
+    {
+        exit(1);
+    }
+    auto vectorReader = Helper::VectorSetReader::CreateInstance(options);
+    if (ErrorCode::Success != vectorReader->LoadFile(options->m_inputFiles))
+    {
+        LOG(Helper::LogLevel::LL_Error, "Failed to read input file.\n");
+        exit(1);
+    }
+    switch (options->m_quantizerType)
+    {
+    case QuantizerType::None:
+    {
+        vectorReader->GetVectorSet()->Save(options->m_outputFile);
+        vectorReader->GetMetadataSet()->SaveMetadata(options->m_outputMetadataFile, options->m_outputMetadataIndexFile);
+        break;
+    }
+    case QuantizerType::PQQuantizer:
+    {
+        vectorReader->GetMetadataSet()->SaveMetadata(options->m_outputMetadataFile, options->m_outputMetadataIndexFile);
+        std::shared_ptr<VectorSet> quantized_vectors;
+        switch (options->m_inputValueType)
+        {
+#define DefineVectorValueType(Name, Type) \
+                    case VectorValueType::Name: \
+                        COMMON::DistanceUtils::Quantizer.reset(new COMMON::PQQuantizer<Type>(options->m_quantizedDim, 256, (DimensionType)(options->m_dimension/options->m_quantizedDim), false, TrainPQQuantizer<Type>(options, vectorReader->GetVectorSet(), quantized_vectors))); \
+                        break;
 
+#include "inc/Core/DefinitionList.h"
+#undef DefineVectorValueType
+        }
+        auto ptr = SPTAG::f_createIO();
+        if (ptr != nullptr && ptr->Initialize(options->m_outputQuantizerFile.c_str(), std::ios::binary | std::ios::out))
+        {
+            if (ErrorCode::Success != COMMON::DistanceUtils::Quantizer->SaveQuantizer(ptr))
+            {
+                LOG(Helper::LogLevel::LL_Error, "Failed to write quantizer file.\n");
+                exit(1);
+            }
+        }
+        else
+        {
+            LOG(Helper::LogLevel::LL_Error, "Failed to open quantizer file.\n");
+            exit(1);
+        }
+
+        if (ErrorCode::Success != quantized_vectors->Save(options->m_outputQuantizerFile))
+        {
+            LOG(Helper::LogLevel::LL_Error, "Failed to save quantized vectors.\n");
+            exit(1);
+        }
+        
+        break;
+    }
+    default:
+    {
+        LOG(Helper::LogLevel::LL_Error, "Failed to read quantizer type.\n");
+        exit(1);
+    }
+    }
 }
