99ce4709e9198b2563a0f34efa3297923de3a916
Christian Cosgrove <christiancosgrove@users.noreply.github.com>
2021-12-21 18:05:38 -0800

    Fix build on Linux by including thread (#252)

--- AnnService/src/BalancedDataPartition/main.cpp
+++ AnnService/src/BalancedDataPartition/main.cpp
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
+#ifdef __linux__
+#include <thread>
+#endif
 #include <mpi.h>
 #include <cstdlib>
 #include <algorithm>
