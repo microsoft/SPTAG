8293a167ade40df7fba33223fe6cc0a76e84e0dc
chenqi <cheqi@microsoft.com>
2022-03-02 07:01:37 +0000

    add DiskANN

---
+++ AnnService/inc/Core/DiskANN/Index.h
@@ -0,0 +1,106 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#ifndef _SPTAG_DISKANN_INDEX_H_
+#define _SPTAG_DISKANN_INDEX_H_
+
+#include "../Common.h"
+#include "../VectorIndex.h"
+#include "DiskANN/include/index.h"
+
+namespace SPTAG
+{
+
+    namespace Helper
+    {
+        class IniReader;
+    }
+
+    namespace DiskANN
+    {
+        template<typename T>
+        class Index : public VectorIndex
+        {
+        private:
+
+
+        public:
+            Index()
+            {
+            }
+
+            ~Index() {}
+
+            inline std::shared_ptr<VectorIndex> GetMemoryIndex() { return m_index; }
+            inline std::shared_ptr<IExtraSearcher> GetDiskIndex() { return m_extraSearcher; }
+            inline Options* GetOptions() { return &m_options; }
+
+            inline SizeType GetNumSamples() const { return m_options.m_vectorSize; }
+            inline DimensionType GetFeatureDim() const { return m_options.m_dim; }
+
+            inline int GetCurrMaxCheck() const { return m_options.m_maxCheck; }
+            inline int GetNumThreads() const { return m_options.m_iSSDNumberOfThreads; }
+            inline DistCalcMethod GetDistCalcMethod() const { return m_options.m_distCalcMethod; }
+            inline IndexAlgoType GetIndexAlgoType() const { return IndexAlgoType::SPANN; }
+            inline VectorValueType GetVectorValueType() const { return GetEnumValueType<T>(); }
+
+            inline float AccurateDistance(const void* pX, const void* pY) const {
+                if (m_options.m_distCalcMethod == DistCalcMethod::L2) return m_fComputeDistance((const T*)pX, (const T*)pY, m_options.m_dim);
+
+                float xy = m_iBaseSquare - m_fComputeDistance((const T*)pX, (const T*)pY, m_options.m_dim);
+                float xx = m_iBaseSquare - m_fComputeDistance((const T*)pX, (const T*)pX, m_options.m_dim);
+                float yy = m_iBaseSquare - m_fComputeDistance((const T*)pY, (const T*)pY, m_options.m_dim);
+                return 1.0f - xy / (sqrt(xx) * sqrt(yy));
+            }
+            inline float ComputeDistance(const void* pX, const void* pY) const { return m_fComputeDistance((const T*)pX, (const T*)pY, m_options.m_dim); }
+            inline bool ContainSample(const SizeType idx) const { return idx < m_options.m_vectorSize; }
+
+            std::shared_ptr<std::vector<std::uint64_t>> BufferSize() const
+            {
+                std::shared_ptr<std::vector<std::uint64_t>> buffersize(new std::vector<std::uint64_t>);
+                auto headIndexBufferSize = m_index->BufferSize();
+                buffersize->insert(buffersize->end(), headIndexBufferSize->begin(), headIndexBufferSize->end());
+                buffersize->push_back(sizeof(long long) * m_index->GetNumSamples());
+                return std::move(buffersize);
+            }
+
+            std::shared_ptr<std::vector<std::string>> GetIndexFiles() const
+            {
+                std::shared_ptr<std::vector<std::string>> files(new std::vector<std::string>);
+                auto headfiles = m_index->GetIndexFiles();
+                for (auto file : *headfiles) {
+                    files->push_back(m_options.m_headIndexFolder + FolderSep + file);
+                }
+                files->push_back(m_options.m_headIDFile);
+                return std::move(files);
+            }
+
+            ErrorCode SaveConfig(std::shared_ptr<Helper::DiskPriorityIO> p_configout); // *
+            ErrorCode SaveIndexData(const std::vector<std::shared_ptr<Helper::DiskPriorityIO>>& p_indexStreams); // *
+
+            ErrorCode LoadConfig(Helper::IniReader& p_reader); // *
+            ErrorCode LoadIndexData(const std::vector<std::shared_ptr<Helper::DiskPriorityIO>>& p_indexStreams); // *
+            ErrorCode LoadIndexDataFromMemory(const std::vector<ByteArray>& p_indexBlobs) { return ErrorCode::Undefined; }
+
+            ErrorCode BuildIndex(const void* p_data, SizeType p_vectorNum, DimensionType p_dimension, bool p_normalized = false); // *
+            ErrorCode BuildIndex(bool p_normalized = false); // *
+            ErrorCode SearchIndex(QueryResult& p_query, bool p_searchDeleted = false) const; // *
+
+            ErrorCode SetParameter(const char* p_param, const char* p_value, const char* p_section = nullptr); // *
+            std::string GetParameter(const char* p_param, const char* p_section = nullptr) const; // *
+
+            inline const void* GetSample(const SizeType idx) const { return nullptr; }
+            inline SizeType GetNumDeleted() const { return 0; }
+            inline bool NeedRefine() const { return false; }
+
+            ErrorCode RefineSearchIndex(QueryResult& p_query, bool p_searchDeleted = false) const { return ErrorCode::Undefined; }
+            ErrorCode SearchTree(QueryResult& p_query) const { return ErrorCode::Undefined; }
+            ErrorCode AddIndex(const void* p_data, SizeType p_vectorNum, DimensionType p_dimension, std::shared_ptr<MetadataSet> p_metadataSet, bool p_withMetaIndex = false, bool p_normalized = false) { return ErrorCode::Undefined; }
+            ErrorCode DeleteIndex(const void* p_vectors, SizeType p_vectorNum) { return ErrorCode::Undefined; }
+            ErrorCode DeleteIndex(const SizeType& p_id) { return ErrorCode::Undefined; }
+            ErrorCode RefineIndex(const std::vector<std::shared_ptr<Helper::DiskPriorityIO>>& p_indexStreams, IAbortOperation* p_abort) { return ErrorCode::Undefined; }
+            ErrorCode RefineIndex(std::shared_ptr<VectorIndex>& p_newIndex) { return ErrorCode::Undefined; }
+        };
+    } // namespace DiskANN
+} // namespace SPTAG
+#endif // _SPTAG_DISKANN_INDEX_H_
