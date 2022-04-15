5ef6dbc9a378303cb398bb4da241419f61cec6de
Philip Adams <philipadams@microsoft.com>
2022-04-14 17:20:37 -0700

    Clean up some dead code in selecthead

--- AnnService/inc/SSDServing/SelectHead.h
+++ AnnService/inc/SSDServing/SelectHead.h
@@ -860,6 +860,146 @@ namespace SPTAG {
                     LOG(Helper::LogLevel::LL_Info, "Set SplitFactor to %d\n", p_opts.m_splitFactor);
                 }
             }
+
+            ErrorCode Bootstrap(SPANN::Options& opts) {
+
+                Utils::StopW sw;
+
+                LOG(Helper::LogLevel::LL_Info, "Start loading vector file.\n");
+                auto valueType = COMMON::DistanceUtils::Quantizer ? SPTAG::VectorValueType::UInt8 : opts.m_valueType;
+                std::shared_ptr<Helper::ReaderOptions> options(new Helper::ReaderOptions(valueType, opts.m_dim, opts.m_vectorType, opts.m_vectorDelimiter));
+                auto vectorReader = Helper::VectorSetReader::CreateInstance(options);
+                if (ErrorCode::Success != vectorReader->LoadFile(opts.m_vectorPath))
+                {
+                    LOG(Helper::LogLevel::LL_Error, "Failed to read vector file.\n");
+                    exit(1);
+                }
+                auto vectorSet = vectorReader->GetVectorSet();
+                if (opts.m_distCalcMethod == DistCalcMethod::Cosine) vectorSet->Normalize(opts.m_iSelectHeadNumberOfThreads);
+                LOG(Helper::LogLevel::LL_Info, "Finish loading vector file.\n");
+
+                AdjustOptions(opts, vectorSet->Count());
+
+                std::vector<int> selected;
+
+                if (Helper::StrUtils::StrEqualIgnoreCase(opts.m_selectType.c_str(), "Random")) {
+                    LOG(Helper::LogLevel::LL_Info, "Start generating Random head.\n");
+                    selected.resize(vectorSet->Count());
+                    for (int i = 0; i < vectorSet->Count(); i++) selected[i] = i;
+                    std::shuffle(selected.begin(), selected.end(), rg);
+
+                    int headCnt = static_cast<int>(std::round(opts.m_ratio * vectorSet->Count()));
+                    selected.resize(headCnt);
+
+                }
+                else if (Helper::StrUtils::StrEqualIgnoreCase(opts.m_selectType.c_str(), "Clustering")) {
+                    LOG(Helper::LogLevel::LL_Info, "Start generating Clustering head.\n");
+                    int headCnt = static_cast<int>(std::round(opts.m_ratio * vectorSet->Count()));
+
+                    switch (valueType)
+                    {
+#define DefineVectorValueType(Name, Type) \
+    case VectorValueType::Name: \
+        Clustering<Type>(vectorSet, opts, selected, headCnt); \
+		break;
+
+#include "inc/Core/DefinitionList.h"
+#undef DefineVectorValueType
+
+                    default: break;
+                    }
+
+                }
+                else if (Helper::StrUtils::StrEqualIgnoreCase(opts.m_selectType.c_str(), "BKT")) {
+                    LOG(Helper::LogLevel::LL_Info, "Start generating BKT.\n");
+                    std::shared_ptr<COMMON::BKTree> bkt;
+                    switch (valueType)
+                    {
+#define DefineVectorValueType(Name, Type) \
+    case VectorValueType::Name: \
+        bkt = BuildBKT<Type>(vectorSet, opts); \
+		break;
+
+#include "inc/Core/DefinitionList.h"
+#undef DefineVectorValueType
+
+                    default: break;
+                    }
+                    LOG(Helper::LogLevel::LL_Info, "Finish generating BKT.\n");
+
+                    std::unordered_map<int, int> counter;
+
+                    if (opts.m_calcStd)
+                    {
+                        CalcLeafSize(0, bkt, counter);
+                    }
+
+                    if (opts.m_analyzeOnly)
+                    {
+                        LOG(Helper::LogLevel::LL_Info, "Analyze Only.\n");
+
+                        std::vector<BKTNodeInfo> bktNodeInfos(bkt->size());
+
+                        // Always use the first tree
+                        DfsAnalyze(0, bkt, vectorSet, opts, 0, bktNodeInfos);
+
+                        LOG(Helper::LogLevel::LL_Info, "Analyze Finish.\n");
+                    }
+                    else {
+                        if (SelectHead(vectorSet, bkt, opts, counter, selected) != ErrorCode::Success)
+                        {
+                            LOG(Helper::LogLevel::LL_Error, "Failed to select head.\n");
+                            return ErrorCode::Fail;
+                        }
+                    }
+                }
+
+                LOG(Helper::LogLevel::LL_Info,
+                    "Seleted Nodes: %u, about %.2lf%% of total.\n",
+                    static_cast<unsigned int>(selected.size()),
+                    selected.size() * 100.0 / vectorSet->Count());
+
+                if (!opts.m_noOutput)
+                {
+                    std::sort(selected.begin(), selected.end());
+
+                    std::shared_ptr<Helper::DiskPriorityIO> output = SPTAG::f_createIO(), outputIDs = SPTAG::f_createIO();
+                    if (output == nullptr || outputIDs == nullptr ||
+                        !output->Initialize(opts.m_headVectorFile.c_str(), std::ios::binary | std::ios::out) ||
+                        !outputIDs->Initialize(opts.m_headIDFile.c_str(), std::ios::binary | std::ios::out)) {
+                        LOG(Helper::LogLevel::LL_Error, "Failed to create output file:%s %s\n", opts.m_headVectorFile.c_str(), opts.m_headIDFile.c_str());
+                        exit(1);
+                    }
+
+                    SizeType val = static_cast<SizeType>(selected.size());
+                    if (output->WriteBinary(sizeof(val), reinterpret_cast<char*>(&val)) != sizeof(val)) {
+                        LOG(Helper::LogLevel::LL_Error, "Failed to write output file!\n");
+                        exit(1);
+                    }
+                    DimensionType dt = vectorSet->Dimension();
+                    if (output->WriteBinary(sizeof(dt), reinterpret_cast<char*>(&dt)) != sizeof(dt)) {
+                        LOG(Helper::LogLevel::LL_Error, "Failed to write output file!\n");
+                        exit(1);
+                    }
+                    for (auto& ele : selected)
+                    {
+                        uint64_t vid = static_cast<uint64_t>(ele);
+                        if (outputIDs->WriteBinary(sizeof(vid), reinterpret_cast<char*>(&vid)) != sizeof(vid)) {
+                            LOG(Helper::LogLevel::LL_Error, "Failed to write output file!\n");
+                            exit(1);
+                        }
+                        if (output->WriteBinary(vectorSet->PerVectorDataSize(), (char*)(vectorSet->GetVector((SizeType)vid))) != vectorSet->PerVectorDataSize()) {
+                            LOG(Helper::LogLevel::LL_Error, "Failed to write output file!\n");
+                            exit(1);
+                        }
+                    }
+                }
+
+                double elapsedMinutes = sw.getElapsedMin();
+                LOG(Helper::LogLevel::LL_Info, "Total used time: %.2lf minutes (about %.2lf hours).\n", elapsedMinutes, elapsedMinutes / 60.0);
+
+                return ErrorCode::Success;
+            }
 		}
 	}
 }
