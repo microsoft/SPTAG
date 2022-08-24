a6b76047f313c09003919f46d3ee5ecc3a3a43d2
cheqi <cheqi@SRGSSD-07>
2022-08-24 06:54:59 +0000

    add python version in wheel package

--- setup.py
+++ setup.py
@@ -38,6 +38,11 @@ nuget_release = os.environ.get('NUGET_RELEASE')
 python_version = "%d.%d" % (sys.version_info.major, sys.version_info.minor)
 print ("Python version:%s" % python_version)
 
+if 'bdist_wheel' in sys.argv:
+  if not any(arg.startswith('--python-tag') for arg in sys.argv):
+      sys.argv.extend(['--python-tag', 'py%d%d'%(sys.version_info.major, sys.version_info.minor)])
+print (sys.argv)
+
 def _setup():
     setuptools.setup(
         name = 'sptag',
