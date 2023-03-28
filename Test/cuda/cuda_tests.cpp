d4133b5a1ea260b07c60c12b44dfb6798872ba24
Ben Karsin <bkarsin@nvidia.com>
2023-03-27 17:17:52 -1000

    Initial unit tests and KNN build test

---
+++ Test/cuda/cuda_tests.cpp
@@ -0,0 +1,26 @@
+//#include "test_kernels.cu"
+
+#define BOOST_TEST_MODULE GPU
+
+#include <cstdlib>
+#include <chrono>
+
+#include <iostream>
+#include <boost/test/included/unit_test.hpp>
+#include <boost/filesystem.hpp>
+
+int GPUTestRandom_All();
+
+BOOST_AUTO_TEST_CASE(RandomTests) {
+  BOOST_CHECK(1 == 1);
+
+  int errors = GPUTestRandom_All();
+  BOOST_CHECK(errors == 0);
+}
+
+int GPUTestDistance_All(); 
+
+BOOST_AUTO_TEST_CASE(DistanceTests) {
+  int success = GPUTestDistance_All();
+  BOOST_CHECK(success == 1);
+}
